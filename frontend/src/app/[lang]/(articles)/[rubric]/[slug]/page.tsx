badfddaffafe0c4650604743a9c1fa4aa05af39a
AleksyKuczynski <alehkuczynski@gmail.com>
2026-04-05 19:44:57 +0200

    update: article page error handling

--- frontend/src/app/[lang]/(articles)/[rubric]/[slug]/page.tsx
+++ frontend/src/app/[lang]/(articles)/[rubric]/[slug]/page.tsx
@@ -105,7 +105,7 @@ export default async function ArticlePage({
 
   const articleSlug = await resolveArticleSlug(slug, lang);
   if (!articleSlug) {
-    throw new Error('Article not found');
+    notFound();
   }
 
   try {
