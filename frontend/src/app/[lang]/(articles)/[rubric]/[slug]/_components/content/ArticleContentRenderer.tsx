fdd592b34f799cd3a5efcf55994c1b932860d6ba
AleksyKuczynski <alehkuczynski@gmail.com>
2026-06-27 09:50:05 +0200

    upd: InlineArticleCard dictionary entry and handling

--- frontend/src/app/[lang]/(articles)/[rubric]/[slug]/_components/content/ArticleContentRenderer.tsx
+++ frontend/src/app/[lang]/(articles)/[rubric]/[slug]/_components/content/ArticleContentRenderer.tsx
@@ -72,6 +72,7 @@ export default function ArticleContentRenderer({
           <InlineArticleCard
             key={index}
             articleCardData={chunk.articleCardData}
+            lang={lang}
           />
         ) : null;
 
